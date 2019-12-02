56416314e8b2c2ddaabe4a9eb8cbd990b0dd03b0
adam-poulton <adam.poulton5@gmail.com>
2019-12-02 23:12:59 +1100

    Add calculator interface skeleton

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
  * @description: Calculator implementation
  */
 
- 
+
 const multiply = (a, b) => a * b;
 
 const add = (a, b) => a + b;
@@ -19,3 +19,27 @@ const operate = (operand, a, b) => {
   if (operand === "+") return add (a, b);
   if (operand === "-") return subtract (a, b);
 }
+
+const container = document.getElementById('container');
+
+const calculator = document.createElement('div');
+calculator.classList.add('calc-container');
+container.appendChild(calculator);
+
+const display = document.createElement('div');
+display.classList.add('calc-display');
+calculator.appendChild(display);
+
+const buttons = document.createElement('div');
+buttons.classList.add('calc-buttons');
+calculator.appendChild(buttons);
+
+for (let i = 1; i < 25; i++) {
+  let button = document.createElement('div')
+  button.classList.add('calc-button')
+  if (i % 4 !== 0 && i > 8 && (i != 21 || i != 23)) {
+    button.classList.add('calc-num')
+  }
+  buttons.appendChild(button);
+}
+
