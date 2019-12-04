a9d12f76ed872d0cd0ba41517b2482450bcd1942
adam-poulton <adam.poulton5@gmail.com>
2019-12-04 23:08:02 +1100

    Implement Add, Subtract, Divide, Multiply, Equals & Clear on interface

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const S_EQUALS = '\u003D';
 const S_NEGATIVE = '\u002D';
 const S_SUPONE = '\u00B9';
 const S_SUPPLUS = '\u207A';
-const disabled = true;
 const symbols = [
   {disabled: true, icon: S_PERCENT, action: "percent"},
   {disabled: true, icon: `${S_SQRT}x`, action: "sqrt"},
@@ -23,23 +22,23 @@ const symbols = [
   {disabled: true, icon: "CE", action: "cleare"},
   {disabled: false, icon: "C", action: "clear"},
   {disabled: true, icon: "B", action: "backspace"},
-  {disabled: true, icon: `${S_DIVIDE}`, action: "divide"},
+  {disabled: false, icon: `${S_DIVIDE}`, action: "divide"},
   {disabled: false, icon: "7", action: '7'},
   {disabled: false, icon: "8", action: '8'},
   {disabled: false, icon: "9", action: '9'},
-  {disabled: true, icon: `${S_MULTIPLY}`, action: "multiply"},
+  {disabled: false, icon: `${S_MULTIPLY}`, action: "multiply"},
   {disabled: false, icon: "4", action: '4'},
   {disabled: false, icon: "5", action: '5'},
   {disabled: false, icon: "6", action: '6'},
-  {disabled: true, icon: `${S_SUBTRACT}`, action: "subtract"},
+  {disabled: false, icon: `${S_SUBTRACT}`, action: "subtract"},
   {disabled: false, icon: "1", action: '1'},
   {disabled: false, icon: "2", action: '2'},
   {disabled: false, icon: "3", action: '3'},
-  {disabled: true, icon: `${S_PLUS}`, action: "add"},
+  {disabled: false, icon: `${S_PLUS}`, action: "add"},
   {disabled: true, icon: `${S_SUPPLUS}${S_FRACTION}${S_NEGATIVE}`, action: "negate"},
   {disabled: false, icon: "0", action: '0'},
-  {disabled: true, icon: ".", action: "decimal"},
-  {disabled: true, icon: `${S_EQUALS}`, action: "equals"},
+  {disabled: false, icon: ".", action: "."},
+  {disabled: false, icon: `${S_EQUALS}`, action: "equals"},
 ];
 
 const peek = (aList) => aList.length > 0 ? aList[aList.length-1] : undefined;
@@ -68,7 +67,21 @@ const inverseString = (x) => `1${S_FRACTION} ${x}`;
 const negate = (x) => -x;
 const negateString = (x) => `negate( ${x} )`;
 
+const equals = (x, y) => y;
 
+const runningDisplayString = (lastDisplay, operator, replace=false) => {
+  let string = String(lastDisplay)
+  // If replacing an existing operator, slice off last two characters
+  if (replace) {
+    string = string.slice(0,-2);
+  }
+  if (operator === 'add') return addString(string);
+  if (operator === 'subtract') return subtractString(string)
+  if (operator === 'multiply') return multiplyString(string)
+  if (operator === 'divide') return divideString(string)
+  if (operator === 'equals') return "";
+
+}
 
 const operate = (operand, a, b) => {
   // check parameters
@@ -85,44 +98,87 @@ const operate = (operand, a, b) => {
   if (operand === "inverse") return inverse(x);
   if (operand === "square") return square(x);
   if (operand === "sqrt") return sqrt(x);
+  if (operand === "equals" ) return equals(x, y);
 }
 
-let buffer = [0];
-let equationStack = [];
-let currentOperation = '';
-let result = 0;
-let auxResult = '';
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+  runningDisplay: '',
+};
 
-let replaceBufferOnNextNumber = false;
+const resetCalculator = () => {
+  calculator.displayValue = '0';
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+  calculator.runningDisplay = '';
+}
 
+const inputDigit = (digit) => {
+  const {displayValue, waitingForSecondOperand} = calculator;
 
-const addToBuffer = (input) => {
-  // ToDo: check if input & output is a finite number
-
-  // build numbers when buffer is empty or already contains numbers
-  if (buffer.length == 0 || !isNaN(peek(buffer))){
-    buffer.push(input)
-    if (buffer[0] == 0) buffer.shift()
-  } else if(replaceBufferOnNextNumber) {
-    buffer = [input]
+  if (waitingForSecondOperand) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
   }
 }
 
+const inputDecimal = (dot) => {
+  const {displayValue, waitingForSecondOperand} = calculator;
+  // If `displayValue` does not contain a decimal point already
+  if (!displayValue.includes(dot)) {
+    if (waitingForSecondOperand) {
+      calculator.displayValue = '0' + dot;
+      waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue += dot;
+    }
+  }
+
+}
+
+const handleOperator = (nextOperator) => {
+  const {firstOperand, displayValue, operator, waitingForSecondOperand} = calculator;
+  const inputValue = parseFloat(displayValue);
+
+  // If `nextOperator` is overwriting a previously selected operator
+  if (operator && waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+    return
+  }
+
+  if (firstOperand == null) {
+    calculator.firstOperand = inputValue;
+
+  } else if (operator) {
+    const currentValue = firstOperand || 0;
+    const result = operate(operator, currentValue, inputValue);
+
+    calculator.displayValue = String(result);
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+
+  // add a check for operations performed in place. i.e sqrt, square, inverse, negate
+  calculator.operator = nextOperator;
+
+}
+
+
 const clear = () => {
-  buffer = [0];
-  equationStack = [];
-  currentOperation = '';
-  result = 0;
-  auxResult = '';
+  resetCalculator();
   refreshScreen();
 }
 
-const pushBuffer = () => {
-  equationStack.push(buffer.join(''))
-}
-
 const refreshScreen = () => {
-  let result = buffer.join('')
+  let result = calculator.displayValue
+  let auxResult = calculator.runningDisplay
   if (result.length > 10) {
     result = parseFloat(result).toExponential(5)
   }
@@ -134,13 +190,14 @@ const handleInput = (e) => {
   let action = e.target.dataset.action;
   // clear screen
   if (action === 'clear') clear();
-  // addition
-  if (action === 'add') {
-    currentOperation = 'add';
-    replaceBufferOnNextNumber = true;
+  // addition, etc
+  if (['add', 'subtract', 'divide', 'multiply', 'equals'].includes(action)) {
+    handleOperator(action);
   }
+  // decimal place
+  if (action === '.') inputDecimal(action);
   // numerical action
-  if (!isNaN(action)) addToBuffer(action);
+  if (!isNaN(action)) inputDigit(action);
 
   refreshScreen();
   e.target.blur();
@@ -148,16 +205,16 @@ const handleInput = (e) => {
 
 
 const container = document.getElementById('container');
-const calculator = document.createElement('div');
+const calc = document.createElement('div');
 const display = document.createElement('div');
 const outputAux = document.createElement('div');
 const output = document.createElement('div');
 function generateUI() {
-  calculator.classList.add('calc-container');
-  container.appendChild(calculator);
+  calc.classList.add('calc-container');
+  container.appendChild(calc);
 
   display.classList.add('calc-display');
-  calculator.appendChild(display);
+  calc.appendChild(display);
 
   outputAux.classList.add('calc-output-aux');
   display.appendChild(outputAux);
@@ -167,7 +224,7 @@ function generateUI() {
 
   const buttons = document.createElement('div');
   buttons.classList.add('calc-buttons');
-  calculator.appendChild(buttons);
+  calc.appendChild(buttons);
 
   for (let i = 1; i < 25; i++) {
     const data = symbols[i-1];
